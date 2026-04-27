6b811deb7ddbc0bfbdeac3be85244d9e9fb32169
Joshua Adeniran <hardexnizzx442@gmail.com>
2026-04-27 06:58:41 -0700

    judge layer deploy 1.0

--- judge_layer/telemetry.py
+++ judge_layer/telemetry.py
@@ -1,3 +1,4 @@
+import os  # Make sure to import os at the top
 from opentelemetry import trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
@@ -6,16 +7,14 @@ from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExport
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 def init_telemetry(app, service_name):
-    # 1. Setup the Tracer with the specific service name
     resource = Resource.create(attributes={"service.name": service_name})
     provider = TracerProvider(resource=resource)
     
-    # 2. Setup the Exporter to send data to Jaeger
-    # Tip: In production, change 'jaeger' to an environment variable
-    exporter = OTLPSpanExporter(endpoint="http://jaeger:4317", insecure=True)
-    processor = BatchSpanProcessor(exporter)
-    provider.add_span_processor(processor)
-    trace.set_tracer_provider(provider)
+    # Check the environment variable before setting up the exporter
+    if os.getenv("OTEL_TRACES_EXPORTER") != "none":
+        exporter = OTLPSpanExporter(endpoint="http://jaeger:4317", insecure=True)
+        processor = BatchSpanProcessor(exporter)
+        provider.add_span_processor(processor)
     
-    # 3. Instrument the FastAPI app
+    trace.set_tracer_provider(provider)
     FastAPIInstrumentor.instrument_app(app)
